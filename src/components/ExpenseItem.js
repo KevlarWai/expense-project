f92b4d80820c7ec4795af85a8401ecb3c913e925
KevlarWai <kevinwai@hotmail.co.uk>
2021-04-29 19:27:37 +0100

    Pass in data via props
    Remove data hardcoded in ExpenseItem and pass values in using parameters

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2020, 7, 1);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 406.83;
+function ExpenseItem(props) {
+  const expenseDate = props.date;
+  const expenseTitle = props.title;
+  const expenseAmount = props.amount;
 
   return (
     <div className="expense-item">
       <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
-        <div className = "expense-item__price">£{expenseAmount}</div>
+        <div className="expense-item__price">£{expenseAmount}</div>
       </div>
     </div>
   );
