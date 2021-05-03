0095361d60911f32903205d63a526f107d631fc2
KevlarWai <kevinwai@hotmail.co.uk>
2021-05-03 20:51:21 +0100

    New component Expenses
    Separate out ExpenseItem into new component so Expenses is only called once for all expense items, added new styling for expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import "./ExpenseItem.css";
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   const expenseTitle = props.title;
@@ -7,7 +7,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">£{expenseAmount}</div>
