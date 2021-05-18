e8de390f0a955abb406da411d4d47a7f84d259a4
KevlarWai <kevinwai@hotmail.co.uk>
2021-05-18 18:26:09 +0100

    Create new form cover component
    Add new cover component which changes to form to add new expense when clicked

---
+++ src/components/NewExpense/ExpenseCover.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+
+const ExpenseCover = (props) => {
+  const [coverSwitch, setCoverSwitch] = useState(0);
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({ ...userInput, enteredTitle: event.target.value });
+    //   setUserInput((prevState) => {
+    //     return { ...prevState, enteredTitle: event.target.value };
+    //   });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({ ...userInput, enteredAmount: event.target.value });
+    //   setUserInput((prevState) => {
+    //     return { ...prevState, enteredAmount: event.target.value };
+    //   });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({ ...userInput, enteredDate: event.target.value });
+    //   setUserInput((prevState) => {
+    //     return { ...prevState, enteredDate: event.target.value };
+    //   });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.submitData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+    setCoverSwitch(0);
+  };
+
+  const addNewExpenseHandler = (event) => {
+      setCoverSwitch(1);
+      console.log(coverSwitch);
+  };
+  const resetHandler = (event) => {
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+    setCoverSwitch(0);
+    console.log(coverSwitch);
+  };
+
+
+  if (coverSwitch === 0) {
+    return (
+      <form onClick={addNewExpenseHandler}>
+        <button type="button">Add New Expense</button>
+      </form>
+    );
+  }
+
+  return (
+    <form onSubmit={submitHandler} onReset={resetHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="reset">Cancel</button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseCover;
